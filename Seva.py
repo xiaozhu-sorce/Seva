8be76394a93bba605675e229d32c2ea09bcb69d4
Github 864187149@qq.com <864187149@qq.com>
2021-05-15 12:40:23 +0800

    seva

--- Seva.py
+++ Seva.py
@@ -9,6 +9,7 @@ class Seva:
 
         self.settings = Settings()
         self.screen = pygame.display.set_mode((1200,800))
+        self.bg_color = (255,255,255)
         pygame.display.set_caption("Seva")
     
     def run_game(self):
@@ -16,9 +17,12 @@ class Seva:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-                
+            
+            self.screen.fill(self.bg_color)
             pygame.display.flip()
     
+    
+
 if __name__ == '__main__':
     seva = Seva()
     seva.run_game()
