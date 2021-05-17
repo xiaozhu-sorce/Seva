d797ed0a0da5daa68c176146a7d4d1ff32e928c6
huangxinhere <1159390360@qq.com>
2021-05-17 20:04:33 +0800

    board down

--- boards.py
+++ boards.py
@@ -2,13 +2,17 @@ import pygame
 
 
 class Board:
-    def     __init__(self, screen, width, length, x, y):
+    def __init__(self, screen, width, length, x, y, ifWarning=False):
         self.screen = screen
-
-        self.image = pygame.image.load('images/brick/brick2.png')
-        self.image = pygame.transform.scale(self.image, (width, length))
-        # self.screen_rect = screen.get_rect()
-        self.rect = self.image.get_rect()
+        self.ifWarning = ifWarning
+        if ifWarning:
+            self.image_warning = pygame.image.load('images/brick/brick2_warning.png')
+            self.image_warning = pygame.transform.scale(self.image_warning, (width, length))
+            self.rect = self.image_warning.get_rect()
+        else:
+            self.image = pygame.image.load('images/brick/brick2.png')
+            self.image = pygame.transform.scale(self.image, (width, length))
+            self.rect = self.image.get_rect()
 
         self.rect.y = y
         self.rect.x = x
@@ -17,4 +21,7 @@ class Board:
         self.rect.y += 1
 
     def blitme(self):
-        self.screen.blit(self.image, self.rect)
+        if self.ifWarning:
+            self.screen.blit(self.image_warning, self.rect)
+        else:
+            self.screen.blit(self.image, self.rect)
